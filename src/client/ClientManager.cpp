18630efc81c10a3eed9a0fe49d0377fde4b3c11d
jeayoungKim529 <jeay529@gmail.com>
2024-11-21 18:28:12 +0900

    feat : client userName, nickName 유효성 검사 구현

--- src/client/ClientManager.cpp
+++ src/client/ClientManager.cpp
@@ -176,6 +176,18 @@ bool ClientManager::check_pass_client(int fd)
     return tmp_client->check_pass_client();
 }
 
+bool Client::check_name_client(std::string name) const //userName, realName 유효성 확인
+{
+	if (name.length() <= 0)
+		return falsel;
+	if (name[0] == '#') //name 첫 글자가 #인지 확인
+		return false;
+	if (name.length() >= 10) //name 길이 확인
+		return false;
+	if (name.find(" ") == std::string::npos) //name에 공백이 있는지 확인
+		return false;
+}
+
 //
 
 std::string ClientManager::get_readBuf(int fd)
@@ -193,11 +205,3 @@ std::string ClientManager::get_writeBuf(int fd)
     tmp_client = find_client(fd);
     return tmp_client->get_writeBuf();
 }
-
-// std::string ClientManager::print_client(int fd)
-// {
-//     Client tmp_client(fd);
-
-//     std::list<Client>::iterator it = std::find(this->client_list.begin(), this->client_list.end(), tmp_client);
-//     return ((&(*it))->get_nickName());
-// }
