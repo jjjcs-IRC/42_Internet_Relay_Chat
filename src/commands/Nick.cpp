04b57738ba986823533afef2da4627942ff4ef0d
jiminChoi <jimchoi@student.42seoul.co.kr>
2024-11-26 19:19:16 +0900

    fix : NICK 수정

--- src/commands/Nick.cpp
+++ src/commands/Nick.cpp
@@ -16,6 +16,7 @@ Nick::Nick(const Nick &other) {
 
 int Nick::executeCommand(tParams &params, ClientManager &cl, ChannelManager &cn) {
 	
+	std::cout << "Nick executeCommand" << std::endl;
 	Client *client = cl.find_client(params.client_fd);
 	if (client->get_passed() == false) {
 		// `USER` 명령어로 사용자 정보를 등록하기 전에 다른 명령어를 사용하려고 하면 이 에러가 발생합니다.
@@ -31,14 +32,16 @@ int Nick::executeCommand(tParams &params, ClientManager &cl, ChannelManager &cn)
 		// `NICK` 명령어로 전송된 닉네임이 이미 사용 중일 때 발생합니다.
 		throw 433;
 	}
-	else {
-		//클라이언트 닉네임 세팅
-		cl.set_nick_client(params.client_fd, params.tokens[1]);
-		//클라이언트 등록 완료 문구 전송
-		if (client->check_pass_client()) {
-			client->set_passed();
-			throw 1;
-		}
+	//클라이언트 닉네임 세팅
+	if (!cl.set_nick_client(params.client_fd, params.tokens[1])) {
+		// `NICK` 명령어로 전송된 닉네임이 서버에서 허용되지 않을 때 발생합니다.
+		throw 432;
 	}
+	//클라이언트 등록 완료 문구 전송
+	if (client->check_pass_client()) {
+		client->set_passed();
+		throw 1;
+	}
+	std::cout << "Nick executeCommand end" << std::endl;
 	return 0;
 }
