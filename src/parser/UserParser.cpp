295bd61d8d6baaec233869369b1d141dba7b8dc4
JaeYeul Jhang <jaeyeuljhang@JaeYeuls-MacBook-Pro.local>
2024-11-26 17:43:11 +0900

    UserParser edit

--- src/parser/UserParser.cpp
+++ src/parser/UserParser.cpp
@@ -12,9 +12,7 @@ UserParser::~UserParser( void )
 
 int	UserParser::CmdParser( void )
 {
-	if (tokens.size()  == 5) // The variable must be number 5
-		throw (461);
-	if (tokens[4][0] != ':') // The last variable must start with ':'
+	if (tokens.size() < 5) // The variable must be number 5
 		throw (461);
 	return (0);
 }
