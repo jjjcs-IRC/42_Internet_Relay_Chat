48d0840d3004d61f9c94bb7b3f446efaac8d5b20
MatSalt <saltey12848@gmail.com>
2024-11-27 18:19:12 +0900

    parser_tester의 main 주석 처리

--- src/parser/parser_tester.cpp
+++ src/parser/parser_tester.cpp
@@ -1,20 +1,20 @@
 #include "IrcParser.hpp"
 
-int	main(void)
-{
-	IrcParser test;
-	std::string	input = ": "; // put the case;
-	tParams	res;
+// int	main(void)
+// {
+// 	IrcParser test;
+// 	std::string	input = ": "; // put the case;
+// 	tParams	res;
 	
-	try
-	{
-		/* code */
-		res = test.IrcParsing(3, input);
-		test.ShowParams();
-	}
-	catch(int a)
-	{
-		std::cerr << "error: " << a << '\n';
-	}
-	return (0);
-}
+// 	try
+// 	{
+// 		/* code */
+// 		res = test.IrcParsing(3, input);
+// 		test.ShowParams();
+// 	}
+// 	catch(int a)
+// 	{
+// 		std::cerr << "error: " << a << '\n';
+// 	}
+// 	return (0);
+// }
