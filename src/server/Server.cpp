932c674a44626a28bd8207ff8f1b633b97c5a1a9
MatSalt <saltey12848@gmail.com>
2024-12-10 20:42:28 +0900

    fix:PASS만 했을때, 문제 해결

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -234,7 +234,7 @@ void Server::handleClientData(int clientSock, struct kevent &event)
 				
 				parse.IrcParsing(clientSock, result, res);
 				numerics.setParams(res); // 토큰에서 사용자의 입력값이 reply에 필요함
-				if (res.tokens[0] == "PASS" && res.tokens[1] != this->password)
+				if (res.tokens[0] == "PASS" && res.tokens.size() == 2 && res.tokens[1] != this->password)
 					tem_string.clear(); // PASS의 비밀번호가 서버와 설정된것과 다르면, 같이 들어온 입력값을 초기화 시킴 
 				Command *command = CommandFactory::getInstance()->createCommand(res.cmd_type);
 				std::cout << "before executeCommand : " << res.cmd_type << res.tokens[0] << std::endl;
