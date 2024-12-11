19a7d963c13e2bbffa592b3bc403a044ab87109c
jiminChoi <jimchoi@student.42seoul.co.kr>
2024-12-11 19:01:34 +0900

    fix : pass 없을 때 무한루프 해결

--- src/commands/Nick.cpp
+++ src/commands/Nick.cpp
@@ -22,7 +22,8 @@ int Nick::executeCommand(tParams &params, ClientManager &cl, ChannelManager &cn)
 	if (client->get_passed() == false) {
 		// `USER` 명령어로 사용자 정보를 등록하기 전에 다른 명령어를 사용하려고 하면 이 에러가 발생합니다.
 		std::cout <<"PASS 를 먼저 입력해야함"<<std::endl;
-		throw 451;
+		// throw 451;
+		return 0;
 	}
 	
 	if (params.tokens.size() < 2) {
