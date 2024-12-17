781494c3afd0fca09f6560d05596e658e0887678
jiminChoi <jimchoi@student.42seoul.co.kr>
2024-12-17 21:31:18 +0900

    refactor : 커맨드 불필요한 주석 제거

--- src/commands/Pass/Pass.cpp
+++ src/commands/Pass/Pass.cpp
@@ -24,8 +24,7 @@ int Pass::executeCommand(tParams &params, ClientManager &cl, ChannelManager &cn)
 	if (client->get_passed()) {// password 통과했는지 확인해주는 함수
 		throw 462;
 	}
-	if (cl.get_server_passwd() != params.tokens[1]) {
-		// `PASS` 명령어로 전송된 비밀번호가 서버에 설정된 비밀번호와 일치하지 않을 때 발생합니다.
+	if (cl.get_server_passwd() != params.tokens[1]) {// `PASS` 명령어로 전송된 비밀번호가 서버에 설정된 비밀번호와 일치하지 않을 때 발생합니다.
 		throw 464;
 	}
 	else {
--- src/commands/User/User.cpp
+++ src/commands/User/User.cpp
@@ -17,22 +17,17 @@ int User::executeCommand(tParams &params, ClientManager &cl, ChannelManager &cn)
 	
 	Client *client = cl.find_client(params.client_fd);
 	if (client->get_passed() == false) {
-		// `USER` 명령어로 사용자 정보를 등록하기 전에 다른 명령어를 사용하려고 하면 이 에러가 발생합니다.
-		std::cout <<"PASS 를 먼저 입력해야함"<<std::endl;
-		// throw 451;
 		return 0;
 	}
 	else if(params.tokens.size() < 5){ //매개변수가 충분하지 않음
 		throw 461;
 	}
-	else if (client->get_realName().size() > 0 || client->get_realName().size() > 0) {
-		//   "You may not reregister" 재등록 하려고 할 때
+	else if (client->get_realName().size() > 0 || client->get_realName().size() > 0) {//   "You may not reregister" 재등록 하려고 할 때
 		throw 462;
 	}
 	else {
 		client->set_userName(params.tokens[1]);
 		client->set_realName(params.tokens[4]);
-		
 		//클라이언트 등록 완료 문구 전송
 		if (client->check_pass_client()) {
 			client->set_passed();
